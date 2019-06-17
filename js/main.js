56f1d23e355ebc71810d5cf596f7f99ba81c71c2
Ankita-Arya <ankitaaryabhalla@gmail.com>
2019-06-17 16:58:50 +0530

    unnecessary code removed

--- js/main.js
+++ js/main.js
@@ -31,26 +31,15 @@ function decrementColumns() {
 	init();
 }
 
-var cubex = 0,    // initial rotation
+var cubex = 0,
 	cubey = 0,
 	cubez = 0;
-function rotate(variableName, degrees) {
-	window[variableName] = window[variableName] + degrees;
-	rotCube(cubex, cubey, cubez);
-}
+
 function rotCube(degx, degy, degz) {
 	segs = "rotateX(" + degx + "deg) rotateY(" + degy + "deg) rotateZ(" + degz + "deg)";
 	document.getElementById('cubeDiv').style.transform = segs;
 }
-function turnRight() {
-	rotate("cubey", 90);
-}
-function turnLeft() {
-	rotate("cubey", -90);
-}
-function flipCube() {
-	rotate("cubez", -180);
-}
+
 
 function throttle(fn, threshhold, scope) {
 	threshhold || (threshhold = 250);
